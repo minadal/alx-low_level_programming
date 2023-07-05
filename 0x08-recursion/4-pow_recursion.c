8a5361e4c0a16239013ca0a3f63061e97beca29f
minadal <amina.daldoul132@gmail.com>
2023-07-05 02:56:47 -0700

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,12 @@
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ *
+ * @x: first int
+ * @y: second int
+ *
+ * Return: int
+ */
+int _pow_recursion(int x, int y)
+{
+	return (y == 0 ? 1 : y < 0 ? -1 : x * _pow_recursion(x, y - 1));
+}
