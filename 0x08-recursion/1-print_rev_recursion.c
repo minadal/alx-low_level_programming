6f14b47ab58c466f21e02b90fd176cd578409e2a
minadal <amina.daldoul132@gmail.com>
2023-07-05 02:25:40 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in inverse
+ *
+ * @s: the string to print
+ *
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
