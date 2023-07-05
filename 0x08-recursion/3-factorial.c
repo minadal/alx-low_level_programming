8dbb4d6a81b7440adebbb4368183c3715ac42b28
minadal <amina.daldoul132@gmail.com>
2023-07-05 02:45:20 -0700

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+/**
+ * factorial - return the factorial of a given num
+ *
+ * @n: the num
+ *
+ * Return: int
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	if (!n)
+	{
+		return (1);
+	}
+
+	return (n * factorial(n - 1));
+}
